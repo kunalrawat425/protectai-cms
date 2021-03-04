8ecccb2df6e3c9231d466f3bc97b6616b4c35e18
mkljngd <mkljngd@gmail.com>
2021-03-04 18:30:41 +0530

    added check with message

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Email;
+use DB;
 
 use App\Http\Requests;
 
@@ -11,15 +12,16 @@ class MailController extends Controller
 {
     public function subscribe(Request $request)
     {
-        $p = new Email;
-
-        $p -> email = $request->email;
-        $p -> save();
-
-        return response()->json([
-            'name' => 'Abigail',
-            'state' => 'CA'
-        ]);
+        $query = DB::select('select * from emails where email = ?',[$request->email]);
+        if(!$query){
+            $p = new Email;
+            $p -> email = $request->email;
+            $p -> save();
+            return response()->json(['message' => 'Subscribed']);
+        }
+        else{
+            return response()->json(['message' => 'Already Subscribed']);
+        }
 
         // return redirect()->away('https://v2.protectt.ai/#/');
 
